de91668f672dc1ab7f7a8e415bb71ab6876ac4b8
sandreas <2050604+sandreas@users.noreply.github.com>
2025-02-11 18:48:26 +0100

    added NestedHistoryRouter sample

---
+++ samples/NestedHistoryRouter/NestedHistoryRouter/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NestedHistoryRouter.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
